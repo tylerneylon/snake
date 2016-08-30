19f0087361d9ffaa1ab123276ed4db7a566c0766
Tyler Neylon <tylerneylon@gmail.com>
2016-08-29 18:16:26 -0700

    Tweak the streaks.

--- intro.lua
+++ intro.lua
@@ -10,7 +10,7 @@ local intro = {}
 -- Internal globals.
 
 local w, h
-local s = '-------------------=======#'
+local s = '-----------------------------==============#'
 
 
 -- Internal functions.
@@ -46,45 +46,45 @@ local function draw_streak(opts)
     w, h = scrsize()
   end
 
-  local x_right    = opts.x_right
-  local y_offset   = opts.y_offset
+  -- Parse out opts.
+  local x_right  = opts.x_right
+  local y_offset = opts.y_offset
+  local prefix   = opts.prefix or 0
+
   local is_pos_set = false
 
   -- We'll draw a string with a certain grayscale curve.
   -- The grayscale colors have codes 232-255; that's 24 values.
   -- We'll let the value 25 indicate full white, which is 231.
   set_color('b', 0)
-  for i = 1, #s do
+  for i = 1 - prefix, #s do
     local x = x_right - #s + i
-    if x > 0 and x < w then
+    if 0 <= x and x < w then
       if not is_pos_set then
         local y_mid = math.floor(h / 2)
-        set_pos(x - 1, y_mid + y_offset)
+        set_pos(x, y_mid + y_offset)
         is_pos_set = true
         set_color('b', 0)
+      end
+      if i < 1 then
         io.write(' ')
-      end
-      local perc  = i / #s
-      local color = math.floor(24 * perc)  -- Values [0, 24].
-      if color == 24 then
-        --set_color('b', 231)  -- Full white background (for the space).
-        set_color('f', 231)  -- Full white.
       else
-        set_color('f', 232 + color)
+        local perc  = i / #s
+        local color = math.floor(24 * perc)  -- Values [0, 24].
+        if color == 24 then
+          --set_color('b', 231)  -- Full white background (for the space).
+          set_color('f', 231)  -- Full white.
+        else
+          set_color('f', 232 + color)
+        end
+        io.write(s:sub(i, i))
       end
-      io.write(s:sub(i, i))
     end
   end
   io.flush()
 end
 
-
--- Public functions.
-
-function intro.go()
-
-  local w, h = scrsize()
-
+local function intro1()
   local timestep = 0.05
 
   for color = 232, 255 do
@@ -99,12 +99,22 @@ function intro.go()
     io.flush()
     sleep(timestep)
   end
+end
+
+
+-- Public functions.
+
+function intro.go()
+
+  local w, h = scrsize()
 
   -- This is temporary work on a new introduction animation.
   clr()
-  for x_right = 0, w + #s + 20 + 2 do  -- + 2 to make sure it goes fully past.
-    draw_streak{x_right = x_right,      y_offset = -1}
-    draw_streak{x_right = x_right - 20, y_offset =  1}
+  local dx  =   3
+  local lag = 200  -- Lag of the second streaker.
+  for x_right = 0, w + #s + lag + dx, dx do
+    draw_streak{x_right = x_right,       y_offset = -1, prefix = dx}
+    draw_streak{x_right = x_right - lag, y_offset =  1, prefix = dx}
     sleep(0.001)
   end
   sleep(1)
